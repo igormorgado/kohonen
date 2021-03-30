8a4305fc34b4995c155d72d02728a3080cb5c6ce
Igor Morgado <morgado.igor@gmail.com>
2021-03-29 23:45:53 -0300

    Fullness matrix plot from arguments

--- src/fullmat.py
+++ src/fullmat.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
+import os
+import sys
 import pandas as pd
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-import sys
 
 # Constantes de analise de qualidade do dado
 
@@ -46,39 +46,20 @@ def fullness_ratio(df):
     return total_data_fullness(df[fields]) / internal_data_fullness(df[fields])
 
 
-def fullness_matrix_plot(fmatrix):
-
-    # Sort matrix by column usage.
-    columns_usage = fmatrix.notna().sum()
-    columns_sorted = columns_usage.sort_values(ascending=False)
-    fmatrix_sorted = fmatrix[columns_sorted.index]
-
-    with open("outputs/attribute_presence.txt", "w") as fd:
-        for k, v in columns_sorted.iteritems():
-            fd.write(f"{k:4s}: {v:3d}\n")
-    fd.close()
-
+def fullness_matrix_plot(fmatrix,figname='outputs/fullness_matrix.png'):
     plt.figure(figsize=(21,7), dpi=150)
     plt.pcolor(fmatrix, shading='flat', edgecolors='black', cmap='cividis')
-    plt.yticks(np.arange(0.5, len(fmatrix_sorted.index), 1), fmatrix_sorted.index, fontsize=4)
-    plt.xticks(np.arange(0.5, len(fmatrix_sorted.columns), 1), fmatrix_sorted.columns, fontsize=4, rotation=90)
+    plt.yticks(np.arange(0.5, len(fmatrix.index), 1), fmatrix.index, fontsize=4)
+    plt.xticks(np.arange(0.5, len(fmatrix.columns), 1), fmatrix.columns, fontsize=4, rotation=90)
     plt.colorbar(label='Fullness',pad=0.01)
     plt.title("Fullness matrix")
     plt.xlabel("Attributes")
     plt.ylabel("Well name")
     plt.tight_layout()
-    plt.savefig('outputs/fullness_matrix.png', dpi=300, bbox_inches='tight')
+    plt.savefig(figname, dpi=300, bbox_inches='tight')
 
 
-def main():
-    # User filenames from command arguments?
-    if len(sys.argv) < 2:
-        print("Usage: {sys.argv[0]} [pandas_well_files] ...")
-        sys.exit()
-
-    files = sys.argv[1:]
-
-    # Compute fullness matrix for files in files
+def fullness_matrix_build(files):
     fullness_matrix = pd.DataFrame()
     for f in files:
         df = pd.read_pickle(f)
@@ -87,11 +68,42 @@ def main():
         fcr = fullness_column_ratio(df)
         fcr.name = wellname
         fullness_matrix = fullness_matrix.append(fcr)
+    return fullness_matrix
 
-    fullness_matrix.sort_index(inplace=True)
+def fullness_matrix_sort_by_attribute(fmatrix):
+    columns_usage = fmatrix.notna().sum()
+    columns_sorted = columns_usage.sort_values(ascending=False)
+    return fmatrix[columns_sorted.index]
+
+    # with open("outputs/attribute_presence.txt", "w") as fd:
+    #     for k, v in columns_sorted.iteritems():
+    #         fd.write(f"{k:4s}: {v:3d}\n")
+    # fd.close()
+
+
+def main():
+    # User filenames from command arguments?
+    if len(sys.argv) < 2:
+        print("Usage: {sys.argv[0]} [pandas_well_files] ... [figname.png]")
+        sys.exit()
+
+    files = sys.argv[1:-1]
+
+    figname = sys.argv[-1]
+    figname = os.path.basename(figname)
+    fignameext = os.path.splitext(figname)[1]
+
+    if (fignameext.lower() != '.png'):
+        print(f"ATTENTION: Last argument MUST be a png filename. Seems {figname} isn't a valid png filename.")
+        sys.exit()
+
+    # Compute fullness matrix for files in files
+    fmat = fullness_matrix_build(files)
+    fmat = fmat.sort_index()
+    fmat = fullness_matrix_sort_by_attribute(fmat)
 
     # Plot the fullness matrix
-    fullness_matrix_plot(fullness_matrix)
+    fullness_matrix_plot(fmat, figname)
 
 if __name__ == "__main__":
     main()
